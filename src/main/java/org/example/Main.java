a74fe15cca0f1551403df4be87f95ff7dc80ba3b
ursulakohler <kohlerursula22@gmail.com>
2024-07-16 10:52:53 +0200

    add date in two weeks and today

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+//Erstellen Sie ein neues Projekt in IntelliJ (bitte verwenden Sie eine Java-Anwendung) und verwenden Sie die java.
+// time-Klassen, um verschiedene Datumsoperationen durchzuführen.
+
+//Schritt 1: Schreiben Sie Code zur Ausgabe des aktuellen Datums und der Uhrzeit.
+
+//Schritt 2: Fügen Sie eine Zeitspanne von 2 Wochen zu dem aktuellen Datum hinzu und geben Sie das neue Datum aus.
+
+//Schritt 3: Vergleichen Sie das aktuelle Datum mit einem angegebenen zukünftigen Datum und geben Sie aus,
+// ob das aktuelle Datum vor oder nach dem angegebenen Datum liegt.
+
+//Schritt 4: Berechnen Sie die Differenz in Tagen zwischen zwei beliebigen Daten und geben Sie das Ergebnis aus.
+
+//ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("Europe/Berlin"));
+//Instant zeitpunkt = Instant.now();
+
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime twoWeeks = today.plusDays(14);
+        System.out.println("Date today: " + today);
+        System.out.println("Date in two weeks: " + twoWeeks);
+
+
+    }
+}
